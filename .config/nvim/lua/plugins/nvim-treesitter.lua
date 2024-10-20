2972e146833358546831d2e76ec0aa8f0fe85ba9
brianhuster <phambinhanctb2004@gmail.com>
2024-10-20 20:35:33 +0700

    update

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,21 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    lazy = false,
-    build = function()
-        vim.cmd('TSInstall all')
-    end,
-    config = function()
-        require 'nvim-treesitter.configs'.setup {
-            highlight = {
-                enable = true,
-            },
-        }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	run = ":TSUpdate",
+	lazy = false,
+	build = function()
+		vim.cmd('TSInstall all')
+	end,
+	config = function()
+		require 'nvim-treesitter.configs'.setup({
+			highlight = {
+				enable = true,
+			},
+		})
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = '*',
+			callback = function()
+				vim.cmd('TSBufEnable highlight')
+			end
+		})
+	end
 }
