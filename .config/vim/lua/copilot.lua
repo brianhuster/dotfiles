10a4d7062a3b4326df2c056283fd95f243912515
brianhuster <phambinhanctb2004@gmail.com>
2024-11-11 17:38:26 +0700

    update

---
+++ .config/vim/lua/copilot.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('i', '<M-CR>', 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
