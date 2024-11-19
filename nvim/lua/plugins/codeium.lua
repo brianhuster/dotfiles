b985aff8f1dce1debff35072a6a217702afbbc7c
brianhuster <phambinhanctb2004@gmail.com>
2024-11-19 16:12:29 +0700

    update

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,17 +1,11 @@
 return {
-	"Exafunction/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		enable_chat = true,
-		enable_cmp_source = false,
-		virtual_text = {
-			-- enabled = true,
-			manual = false,
-			key_bindings = {
-				accept = "<M-CR>",
-			}
-		}
-	}
+	'Exafunction/codeium.vim',
+	config = function()
+		vim.g.codeium_disable_bindings = 1
+		vim.keymap.set('i', '<M-CR>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+		vim.keymap.set('i', '<M-w>', function() return vim.fn['codeium#AcceptNextWord()'](1) end,
+			{ expr = true, silent = true })
+		vim.keymap.set('i', '<M-l>', function() return vim.fn['codeium#AcceptNextLine'](-1) end,
+			{ expr = true, silent = true })
+	end
 }
