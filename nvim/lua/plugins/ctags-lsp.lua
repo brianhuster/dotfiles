fe3da2909823cad713f4efd2a3c3504910c535f3
brianhuster <phambinhanctb2004@gmail.com>
2024-11-26 21:45:24 +0700

    update

---
+++ nvim/lua/plugins/ctags-lsp.lua
@@ -0,0 +1,8 @@
+-- lazy.nvim
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = "netmute/ctags-lsp.nvim",
+	config = function()
+		require("lspconfig").ctags_lsp.setup({})
+	end,
+}
--- nvim/lua/plugins/firenvim.lua
+++ nvim/lua/plugins/firenvim.lua
@@ -1,1 +1,9 @@
-return { 'glacambre/firenvim', build = ":call firenvim#install(0)" }
+return {
+	'glacambre/firenvim',
+	build = function()
+		vim.fn['firenvim#install'](0)
+	end,
+	config = function()
+		vim.g.firenvim_config.localSettings['.*'] = { takeover = 'never' } -- always | never
+	end
+}
