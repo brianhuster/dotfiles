619cf46c5f9258d51fb95b409dd7f6cfb5150b30
brianhuster <phambinhanctb2004@gmail.com>
2024-11-26 21:53:02 +0700

    update

--- nvim/lua/plugins/firenvim.lua
+++ nvim/lua/plugins/firenvim.lua
@@ -4,6 +4,17 @@ return {
 		vim.fn['firenvim#install'](0)
 	end,
 	config = function()
-		vim.g.firenvim_config.localSettings['.*'] = { takeover = 'never' } -- always | never
+		vim.g.firenvim_config = {
+			globalSettings = { alt = "all" },
+			localSettings = {
+				[".*"] = {
+					cmdline  = "neovim",
+					content  = "text",
+					priority = 0,
+					selector = "textarea",
+					takeover = "never"
+				}
+			}
+		}
 	end
 }
