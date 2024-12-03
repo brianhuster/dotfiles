55e63efde2dd202e934439e5ed50e8051bb88a42
brianhuster <phambinhanctb2004@gmail.com>
2024-12-04 01:01:10 +0700

    update

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -6,12 +6,12 @@ vim.o.smartindent = true
 vim.o.cursorline = false
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
-vim.o.history = 5000
 vim.o.clipboard = 'unnamedplus'
 vim.o.autowriteall = true
 vim.o.modeline = false
 vim.o.backspace = 'indent,eol,start'
 vim.g.mapleader = ' '
+vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
 vim.cmd.language({
 	args = { 'vi_VN.utf-8' },
 	mods = { silent = true }
