37e5d4d94174b4affd46bb7764ddf028ef20f732
brianhuster <phambinhanctb2004@gmail.com>
2025-01-13 12:18:45 +0700

    update

--- nvim/lua/plugins/vim-visual-multi.lua
+++ nvim/lua/plugins/vim-visual-multi.lua
@@ -1,1 +1,13 @@
-return { 'mg979/vim-visual-multi' }
+return {
+	'mg979/vim-visual-multi',
+	config = function()
+		vim.cmd [[
+			let g:vm_mouse_mappings    = 1
+			let g:vm_theme             = 'iceblue'
+
+			let g:vm_maps = {}
+			let g:vm_maps["undo"]      = 'u'
+			let g:vm_maps["redo"]      = '<c-r>'
+		]]
+	end
+}
