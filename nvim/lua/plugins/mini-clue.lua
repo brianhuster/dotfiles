31078f3008b493002a1ff615ea7825a3eb9e9b1b
brianhuster <phambinhanctb2004@gmail.com>
2025-01-19 16:58:08 +0700

    update

---
+++ nvim/lua/plugins/mini-clue.lua
@@ -0,0 +1,18 @@
+return {
+	'echasnovski/mini.clue',
+	config = function()
+		local miniclue = require 'mini.clue'
+		require 'mini.clue'.setup {
+			{ mode = 'i', keys = '<C-x>' },
+
+			clues = {
+				-- Enhance this by adding descriptions for <Leader> mapping groups
+				miniclue.gen_clues.builtin_completion(),
+				miniclue.gen_clues.g(),
+				miniclue.gen_clues.marks(),
+				miniclue.gen_clues.registers(),
+				miniclue.gen_clues.windows(),
+				miniclue.gen_clues.z(),
+			}, }
+	end
+}
