5e7aed6c99141f4cc700fc5d03b8fd601e88395c
brianhuster <phambinhanctb2004@gmail.com>
2025-01-20 09:48:38 +0700

    update

--- nvim/colors/an.lua
+++ nvim/colors/an.lua
@@ -1,14 +1,28 @@
-vim.cmd.highlight 'clear'
-vim.o.bg = "dark"
-vim.o.termguicolors = true
-
+local colorscheme = vim.cmd.colorscheme
+local o = vim.o
 local hl = vim.api.nvim_set_hl
 local light_blue_alternative = "#82aaff"
 local bold_grey_alternative = "#FFDAB9"
 
-for _, group in ipairs({ "Identifier", "DiagnosticHint", "DiagnosticUnderlineHint" }) do
+colorscheme "default"
+o.bg = "dark"
+o.termguicolors = true
+
+for _, group in ipairs { "Identifier", "DiagnosticHint", "DiagnosticUnderlineHint" } do
 	hl(0, group, { fg = light_blue_alternative })
 end
-for _, group in ipairs({ "Title", "Statement", "Todo" }) do
+for _, group in ipairs { "Title", "Statement", "Todo" } do
 	hl(0, group, { fg = bold_grey_alternative })
 end
+
+local function au(name, pattern, callback)
+	return vim.api.nvim_create_autocmd(name, { pattern = pattern, callback = callback })
+end
+
+au("OptionSet", { "termguicolors", "background" }, function()
+	if (o.termguicolors and (o.bg == "dark")) then
+		return colorscheme("an")
+	else
+		return colorscheme("default")
+	end
+end)
