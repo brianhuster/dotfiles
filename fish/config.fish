04250e93ffe17fe09bcd21583e6d34d9a276295d
brianhuster <phambinhanctb2004@gmail.com>
2025-04-05 01:29:59 +0700

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x OLLAMA_MODELS "/media/brianhuster/D/.ollama/models"
-	# set local variable "D"
-	set -x D /media/brianhuster/D
-    set -gx PATH $PATH /usr/local/go/bin $D/.android-studio/bin /home/brianhuster/go/bin $D/Android/Sdk/emulator $D/Android/Sdk/platform-tools $D/Android/Sdk/tools
-	set -gx ANDROID_HOME $D/Android/Sdk
+    # set local variable "D"
+    set -x D /media/brianhuster/D
+    set -gx PATH $PATH \
+		$D/.android-studio/bin \
+		~/usr/local/go/bin /go/bin \
+		$D/Android/Sdk/emulator \
+		$D/Android/Sdk/platform-tools \
+		$D/Android/Sdk/tools \
+		~/.local/share/nvim/mason/bin/
+    set -gx ANDROID_HOME $D/Android/Sdk
     set -gx EDITOR nvim
 end
 
