988a83fafd08e54e027f216769db397f4eb11db2
brianhuster <phambinhanctb2004@gmail.com>
2025-06-30 11:31:19 +0700

    add cfilter plugin

--- .config/nvim/plugin/utils.lua
+++ .config/nvim/plugin/utils.lua
@@ -9,16 +9,3 @@ command('GitDiffHead', function()
 	local path = vim.fs.relpath(git_root, api.nvim_buf_get_name(0))
 	vim.cmd(([[tabedit %% | diffthis | vertical new | diffthis | read! git show HEAD^:%s]]):format(path))
 end, { nargs = 0 })
-
-command('Sh', function(opts)
-	vim.cmd.term(opts.args)
-end, {
-	nargs = '+',
-	complete = vim.fn.executable('fish') == 1 and function(_, cmd_line, _)
-		local cmd = table.concat(vim.split(cmd_line, ' '), ' ', 2)
-		local results = vim.fn.systemlist { 'fish', '-c', 'complete -C' .. vim.fn.shellescape(cmd) }
-		return vim.tbl_map(function(result)
-			return vim.split(result, '\t')[1]
-		end, results)
-	end or 'shellcmdline'
-})
