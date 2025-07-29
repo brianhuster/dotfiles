540c130e6c928b827e1a9e192e1eb2641257c82c
brianhuster <phambinhanctb2004@gmail.com>
2025-07-30 01:45:44 +0700

    feat(nonest): support winlayout

--- .config/nvim/plugin/nonest.lua
+++ .config/nvim/plugin/nonest.lua
@@ -17,30 +17,74 @@ if not chan or chan == 0 then
 	vim.cmd("qall!")
 end
 
----@param cmd vim.api.keyset.cmd|string
+---@param cmd string
 local function send_cmd(cmd)
-	if type(cmd) == 'string' then
-		vim.rpcnotify(chan, 'nvim_command', cmd)
-	else
-		vim.rpcnotify(chan, 'nvim_cmd', cmd, {})
+	vim.rpcnotify(chan, 'nvim_command', cmd)
+end
+
+---@type string[]
+local commands = {}
+
+---@param winid integer
+---@param cmd string
+---@param mod? string
+local function add_cmd_for_win(winid, cmd, mod)
+	table.insert(
+		commands,
+		('%s %s %s'):format(
+			mod, cmd,
+			vim.fn.fnameescape(api.nvim_buf_get_name(api.nvim_win_get_buf(winid)))))
+	if vim.wo[winid].diff then
+		table.insert(commands, "diffthis")
+	end
+end
+
+---@param layout vim.fn.winlayout.ret
+---@param is_first boolean
+local function process_winlayout(layout, is_first)
+	local type = layout[1]
+
+	---@param data vim.fn.winlayout.ret[]
+	---@param split_type "vsplit"|"split"
+	---@param first boolean
+	local function process_splits(data, split_type, first)
+		process_winlayout(data[1], first)
+
+		for i = 2, #data do
+			local winid = nil
+			if data[i][1] == 'leaf' then
+				winid = data[i][2]
+			end
+
+			local position = i == #data and "botright" or "belowright"
+
+			add_cmd_for_win(winid --[[@as integer]], split_type, position)
+
+			if data[i][1] ~= 'leaf' then
+				process_winlayout(data[i], false)
+			end
+		end
+	end
+
+	if type == 'leaf' then
+		local winid = layout[2]
+		if is_first then
+			add_cmd_for_win(winid --[[@as integer]], "edit")
+		end
+	elseif type == 'col' then
+		process_splits(layout[2] --[[@as vim.fn.winlayout.ret[] ]], "split", is_first)
+	elseif type == 'row' then
+		process_splits(layout[2] --[[@as vim.fn.winlayout.ret[] ]], "vsplit", is_first)
 	end
 end
 
 api.nvim_create_autocmd('VimEnter', {
 	callback = function()
-		local windows = vim.iter(vim.fn.getwininfo()):filter(function(w)
-			return vim.bo[w.bufnr].buftype == ''
-		end):totable()
+		local winlayout = vim.fn.winlayout()
+		process_winlayout(winlayout, true)
 
-		for i, w in ipairs(windows) do
-			send_cmd {
-				cmd = i == 1 and 'tabedit' or 'vsplit',
-				args = { vim.api.nvim_buf_get_name(w.bufnr) }
-			}
-			if vim.wo[w.winid].diff then
-				send_cmd('diffthis')
-			end
-		end
+		send_cmd('tabnew')
+		vim.iter(commands):each(send_cmd)
 
 		local tabpagenr = vim.rpcrequest(chan, 'nvim_call_function', 'tabpagenr', {}) --[[@as integer]]
 		vim.rpcnotify(chan, 'nvim_create_autocmd', 'TabClosed', {
