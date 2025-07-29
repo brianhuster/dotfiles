d81270bf36cf27a5e7852f2d648904bc15defc2d
brianhuster <phambinhanctb2004@gmail.com>
2025-07-29 11:18:14 +0700

    feat(nonest): support more Nvim flags and args

--- .config/nvim/plugin/nonest.lua
+++ .config/nvim/plugin/nonest.lua
@@ -6,29 +6,76 @@ g.loaded_nonest = true
 
 env.EDITOR = vim.iter(v.argv):map(vim.fn.shellescape):join(' ')
 
----@type 'edit'|'split'|'vsplit'|'tabedit'|'pedit'|'edit!'|'split!'|'vsplit!'|'tabedit!'
-g.nonest_edit_cmd = g.nonest_edit_cmd or 'tabedit'
-
 if not env.NVIM then
 	return
+end
+
+local diffmode = false
+local filenames = {} ---@type string[]
+local args = { unpack(v.argv, 2) } ---@type string[]
+
+do
+	local i = 1
+	local always_filename_args = false
+
+	while i <= #args do
+		local a = args[i]
+		if always_filename_args then
+			vim.list_extend(filenames, { unpack(args, i) })
+			break
+		end
+
+		local handled = false
+		-- Skip all flags that run arbitrary commands
+		if a == '-c' or a == '--cmd' or a == '-S' then
+			i = i + 1
+		else
+			if a == '--' then
+				always_filename_args = true
+			elseif a == '-d' then
+				diffmode = true
+			elseif a:sub(1, 1) ~= '-' and a:sub(1, 1) ~= '+' then
+				table.insert(filenames, a)
+			end
+		end
+		i = i + 1
+	end
 end
 
 local _, chan = pcall(vim.fn.sockconnect, 'pipe', env.NVIM, { rpc = true })
 
 if not chan or chan == 0 then
-	vim.notify('Failed to connect to parent', vim.log.levels.ERROR)
-	vim.cmd('quitall!')
+	io.stderr:write('Nvim failed to connect to parent')
+	vim.cmd("qall!")
+end
+
+---@param cmd vim.api.keyset.cmd|string
+local function send_cmd(cmd)
+	if type(cmd) == 'string' then
+		vim.rpcnotify(chan, 'nvim_command', cmd)
+	else
+		vim.rpcnotify(chan, 'nvim_cmd', cmd, {})
+	end
 end
 
 api.nvim_create_autocmd('VimEnter', {
 	callback = function()
-		local bufname = api.nvim_buf_get_name(0)
-		vim.rpcnotify(chan, 'nvim_command', ("%s %s"):format(g.nonest_edit_cmd, bufname))
-		local parent_buf = vim.rpcrequest(chan, 'nvim_call_function', 'bufnr', { bufname })
-		vim.rpcnotify(chan, 'nvim_create_autocmd', { 'WinClosed', 'BufDelete', 'BufWipeOut', 'TabClosed' }, {
-			buffer = parent_buf,
-			command = ([[call rpcnotify(sockconnect('pipe', '%s', #{ rpc: v:true }), 'nvim_command', 'quitall!')]])
-				:format(v.servername),
+		send_cmd { cmd = 'tabedit', args = { filenames[1] } }
+		if diffmode then
+			send_cmd('diffthis')
+		end
+
+		for i = 2, #filenames do
+			send_cmd { cmd = 'vsplit', args = { filenames[i] } }
+			if diffmode then
+				send_cmd('diffthis')
+			end
+		end
+
+		local tabpagenr = vim.rpcrequest(chan, 'nvim_call_function', 'tabpagenr', {}) --[[@as integer]]
+		vim.rpcnotify(chan, 'nvim_create_autocmd', 'TabClosed', {
+			command = ([[if expand("<afile>") == %s | call rpcnotify(sockconnect('pipe', '%s', #{ rpc: v:true }), 'nvim_command', 'quitall!') | endif]])
+				:format(tabpagenr, v.servername),
 			once = true
 		})
 	end,
