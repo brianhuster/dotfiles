626e8052a40e4caa1fec3b8f9214c4cff98077c0
brianhuster <phambinhanctb2004@gmail.com>
2025-07-29 22:22:06 +0700

    feat: rewrite nonest using windows instead

--- .config/nvim/plugin/nonest.lua
+++ .config/nvim/plugin/nonest.lua
@@ -10,38 +10,6 @@ if not env.NVIM then
 	return
 end
 
-local diffmode = false
-local filenames = {} ---@type string[]
-local args = { unpack(v.argv, 2) } ---@type string[]
-
-do
-	local i = 1
-	local always_filename_args = false
-
-	while i <= #args do
-		local a = args[i]
-		if always_filename_args then
-			vim.list_extend(filenames, { unpack(args, i) })
-			break
-		end
-
-		local handled = false
-		-- Skip all flags that run arbitrary commands
-		if a == '-c' or a == '--cmd' or a == '-S' then
-			i = i + 1
-		else
-			if a == '--' then
-				always_filename_args = true
-			elseif a == '-d' then
-				diffmode = true
-			elseif a:sub(1, 1) ~= '-' and a:sub(1, 1) ~= '+' then
-				table.insert(filenames, a)
-			end
-		end
-		i = i + 1
-	end
-end
-
 local _, chan = pcall(vim.fn.sockconnect, 'pipe', env.NVIM, { rpc = true })
 
 if not chan or chan == 0 then
@@ -60,14 +28,16 @@ end
 
 api.nvim_create_autocmd('VimEnter', {
 	callback = function()
-		send_cmd { cmd = 'tabedit', args = { filenames[1] } }
-		if diffmode then
-			send_cmd('diffthis')
-		end
+		local windows = vim.iter(vim.fn.getwininfo()):filter(function(w)
+			return vim.bo[w.bufnr].buftype == ''
+		end):totable()
 
-		for i = 2, #filenames do
-			send_cmd { cmd = 'vsplit', args = { filenames[i] } }
-			if diffmode then
+		for i, w in ipairs(windows) do
+			send_cmd {
+				cmd = i == 1 and 'tabedit' or 'vsplit',
+				args = { vim.api.nvim_buf_get_name(w.bufnr) }
+			}
+			if vim.wo[w.winid].diff then
 				send_cmd('diffthis')
 			end
 		end
